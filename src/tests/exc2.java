d1556a72f1f9d995bdbbf74bb93fcdbbd0f98e30
outofmakarov <6360058@mail.ru>
2024-10-09 17:18:07 +0300

    exc2

--- src/tests/exc2.java
+++ src/tests/exc2.java
@@ -1,18 +1,36 @@
 package tests;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class exc2 {
     public static void main(String[] args){
         Scanner console = new Scanner(System.in);
-        String input = console.nextLine();
-
-        String[] nums = input.split(" ");
-        Arrays.sort(nums);
-
-        System.out.println(nums[0] + " " + nums[1] + " " + nums[2]);
+        int x = console.nextInt();
+        int y = console.nextInt();
+        int z = console.nextInt();
 
 
+        if (x < y) {
+            if (x < z) {
+                if (y < z) {
+                    System.out.println(x + " " + y + " " + z);
+                } else {
+                    System.out.println(x + " " + z + " " + y);
+                }
+            } else {
+                System.out.println(z + " " + x + " " + y);
+            }
+        } else {
+            if (y < z) {
+                if (x < z) {
+                    System.out.println(y + " " + x + " " + z);
+                } else {
+                    System.out.println(y + " " + z + " " + x);
+                }
+            } else {
+                System.out.println(z + " " + y + " " + x);
+            }
+        }
     }
 }
